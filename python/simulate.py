ef90f25c850f4b8307867ba71ab4943ee1716d87
Vivian Lopez <vivianlopez777@gmail.com>
2025-06-22 01:34:00 +0100

    Fix simulate.py: add headers for pandas dataframe to correctly load data missing headers

--- python/simulate.py
+++ python/simulate.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def run_simulation(csv_path, short_window=3, long_window=5):
     pnl_curve, total_profit = strategy_py.run_strategy(csv_path, short_window, long_window)
-    df = pd.read_csv(csv_path)
+    df = pd.read_csv(csv_path, header=None, names=["Timestamp", "Close"])
     df['PnL'] = pnl_curve
     return df, total_profit
