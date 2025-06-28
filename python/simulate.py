6e3050d984559ba4b18a79437e8ec12021193a55
Vivian Lopez <vivianlopez777@gmail.com>
2025-06-28 17:18:20 +0100

    Remove subprocess code

--- python/simulate.py
+++ python/simulate.py
@@ -1,20 +1,3 @@
-import sys, os, subprocess
-
-# Add C++ module path
-so_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../build/strategy_py.so"))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../build")))
-
-# If strategy_py.so does not exist, run CMake and make
-if not os.path.exists(so_path):
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    repo_root = os.path.abspath(os.path.join(this_dir, ".."))
-
-    build_dir = os.path.join(repo_root, "build")
-    os.makedirs(build_dir, exist_ok=True)
-
-    subprocess.run(["cmake", repo_root], cwd=build_dir, check=True)
-    subprocess.run(["make"], cwd=build_dir, check=True)
-
 import strategy_py
 import pandas as pd
 
